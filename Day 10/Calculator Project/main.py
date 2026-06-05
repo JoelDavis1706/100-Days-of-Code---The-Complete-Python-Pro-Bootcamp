18a073b4358001d0cf2819d78dd9844d9956c5ad
Joel Davis <joeldavis1706@gmail.com>
2026-06-06 02:17:05 +0530

    Adding Function

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -11,3 +11,12 @@ def multiply(n1,n2):
 
 # TODO : Add these 4 functions to dictionary as the values . Keys = "+","-","/","*"
 
+add_operation = add
+sub_operation = subtract
+div_operation = divide
+mul_operation = multiply
+
+
+calculator_functions ={
+    "+":add,"-":subtract,"/":divide,"*":multiply
+}
