57a59441c10db1b14cc40cb6354f1ce20687b513
Joel Davis <joeldavis1706@gmail.com>
2026-06-06 22:14:33 +0530

    Finished calculator, now its working and its using recursion

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,3 +1,5 @@
+import art
+
 def add(n1, n2):
     return n1 + n2
 
@@ -11,12 +13,31 @@ def multiply(n1,n2):
 
 # TODO : Add these 4 functions to dictionary as the values . Keys = "+","-","/","*"
 
-add_operation = add
-sub_operation = subtract
-div_operation = divide
-mul_operation = multiply
-
-
-calculator_functions ={
+operations = {
     "+":add,"-":subtract,"/":divide,"*":multiply
 }
+
+def calculator():
+    print(art.logo)
+    should_acumulate = True
+    number1 = float(input("Enter the first number:"))
+
+    while should_acumulate:
+        for symbols in operations:
+            print(symbols)
+        user_operation = input("Enter the operation you want to use:")
+        number2 = float(input("Enter the second number:"))
+        result = operations[user_operation](number1,number2)
+        print(f"{number1} {user_operation} {number2} = {result}")
+
+        game_continue = input(f"Do you want to continue calculation with {result}, type y for yes, n for no and exit to exit:")
+        if game_continue == "y":
+            number1 = result
+        elif game_continue == "n":
+            should_acumulate = False
+            print("\n" * 50)
+            calculator()
+        elif game_continue == "exit":
+            break
+
+calculator()
