186d41409f8580a564402a6f9ccb5045b858193b
Joel Davis <joeldavis1706@gmail.com>
2026-06-11 01:24:10 +0530

    Succesfully finished Day 12 main.py

--- Day 12/Number Guessing Project/main.py
+++ Day 12/Number Guessing Project/main.py
@@ -2,28 +2,35 @@ import art
 print(art.logo)
 import random
 random_guess = random.randint(1,100)
-def guess_game(game_level):
-    if game_level == "easy":
-
-        for i in range(10):
-            print(f"You have {10-i} attempts remaining.")
-            user_choice = int(input("Enter the number you guessed:"))
-            print(game_check(user_choice))
-
-    if game_level == "hard":
-
-        for i in range(5):
-            print(f"You have {5-i} attempts remaining.")
-            user_choice = int(input("Enter the number you guessed:"))
-            print(game_check(user_choice))
 
 def game_check(check):
     if check > random_guess:
-        return "Too High.\nGuess Again."
+        print("Too High.\nGuess Again.")
+        return False
     elif random_guess > check:
-        return "Too Low.\nGuess Again."
+        print("Too Low.\nGuess Again.")
+        return False
     elif check == random_guess:
-        return "You got the right answer"
+        print("You got the right answer")
+        return True
+
+
+def guess_game(game_level):
+    if game_level == "easy":
+        attempts = 10
+    if game_level == "hard":
+        attempts = 5
+
+    for i in range(attempts):
+        print(f"You have {attempts-i} attempts remaining.")
+        user_choice = int(input("Enter the number you guessed:"))
+        result = game_check(user_choice)
+        if result:
+            break
+    if not result:
+        print(f"Sorry you Lost, the guess was {random_guess}")
+
+
 
 
 print("""Welcome to Number Guessing Game!\nI am thinking of a Number between 1 and 100""")
