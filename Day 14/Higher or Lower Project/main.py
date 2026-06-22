dc0732b73e5f6584f4c68f4b65fd08c09f3c2447
Joel Davis <joeldavis1706@gmail.com>
2026-06-22 23:03:24 +0530

    Added To do's on the Day14 main

---
+++ Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,34 @@
+#Display Art
+import random
+import art
+import game_data
+print(art.logo)
+
+#generate random data from dictionary
+def game_run():
+    print(f"A:{game_data.data[name]},{game_data.data[description]})
+
+
+#Format the account into printable format
+
+#Ask user for a guess
+
+
+#Check if user is correct
+## - Check follower count of each account
+## - Use if statement to check if user is correct
+
+
+#Give user feedback on their guess
+
+#Keeping the score
+
+#make the game repeatable.
+
+#make the account at position at B become the next A
+
+
+
+game_over = False
+while game_over not True:
+    print(game_data[])
