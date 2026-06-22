981cee2dcf148d1554495b8ac359ed27f8170df9
Joel Davis <joeldavis1706@gmail.com>
2026-06-23 04:54:18 +0530

    Added format function on Day 14

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -5,17 +5,26 @@ import game_data
 print(art.logo)
 
 #generate random data from dictionary
-def game_run():
-    print(f"A:{game_data.data[name]},{game_data.data[description]})
-
+def gen_ran():
+    random_option = random.choice(game_data.data)
+    return random_option
 
 #Format the account into printable format
+def format():
+    random_A = gen_ran()
+    random_B = gen_ran()
+    print(f"Compare A:{random_A['name']}, a {random_A['description']}, from {random_A['country']}")
+    print(art.vs)
+    print(f"Against B:{random_B['name']}, a {random_B['description']}, from {random_B['country']}")
 
 #Ask user for a guess
+def ask_guess():
 
 
 #Check if user is correct
+
 ## - Check follower count of each account
+
 ## - Use if statement to check if user is correct
 
 
@@ -29,6 +38,7 @@ def game_run():
 
 
 
-game_over = False
-while game_over not True:
-    print(game_data[])
+# game_over = False
+# while game_over not True:
+#     print(game_data[])
+format()
