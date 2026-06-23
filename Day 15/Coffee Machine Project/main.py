2a23003f18dee8eac56a3c9a724b7c113f437bb4
Joel Davis <joeldavis1706@gmail.com>
2026-06-23 20:51:22 +0530

    Did the structure for Day 15

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,3 +1,5 @@
+from random import choice
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -30,3 +32,41 @@ resources = {
     "coffee": 100,
 }
 
+
+
+money = 0
+def ask_user():
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    return choice
+
+def report():
+    return f"Water: {resources['water']}, Milk: {resources['milk']}, Coffee: {resources['coffee']}"
+
+def insert_coin():
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickels = int(input("How many nickels?: "))
+    pennies = int(input("How many pennies?: "))
+    dollars = (.25 * quarters) + (.10 * dimes) + (.05 * nickels) + (.01 * pennies)
+    return dollars
+
+
+machine_off = False
+print(report())
+print(f"Money: {money}")
+choice = ask_user()
+if choice == "report":
+    print(report())
+elif choice == "off":
+    machine_off = True
+else:
+    print("Please insert coins:")
+    total = round(insert_coin(),2)
+    print(f"Inserted Money: {total}")
+    cost = MENU[choice]["cost"]
+    if cost < total:
+        change = total - cost
+        print(f"Change: {change}")
+        print(f"Here is your {choice}☕ Enjoy!")
+    else:
+        print(f"You don't have enough cash to buy {choice}.")
