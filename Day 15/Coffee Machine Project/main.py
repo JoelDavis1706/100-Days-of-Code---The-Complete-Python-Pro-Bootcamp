647aedbd4ff28dd321f59fa8d51937ef5ac872bf
Joel Davis <joeldavis1706@gmail.com>
2026-06-25 01:07:12 +0530

    Completed day 15 Coffee Machine

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -35,38 +35,58 @@ resources = {
 
 
 money = 0
-def ask_user():
-    choice = input("What would you like? (espresso/latte/cappuccino): ")
-    return choice
+def transaction_succesful(money_recieved,drink_cost):
+    if money_recieved < drink_cost:
+        print(f"Sorry that is not enough money to buy. Money Refunded.")
+        return False
+    change = round(money_recieved - drink_cost,2)
+
+    if change > 0:
+        print(f"Here is your ${change} in change.")
+    return True
 
 def report():
-    return f"Water: {resources['water']}, Milk: {resources['milk']}, Coffee: {resources['coffee']}"
+    print(f"Water: {resources['water']},\nMilk: {resources['milk']},\nCoffee: {resources['coffee']},\nMoney: {money}")
 
 def insert_coin():
+    print("Please insert coins")
     quarters = int(input("How many quarters?: "))
     dimes = int(input("How many dimes?: "))
     nickels = int(input("How many nickels?: "))
     pennies = int(input("How many pennies?: "))
-    dollars = (.25 * quarters) + (.10 * dimes) + (.05 * nickels) + (.01 * pennies)
-    return dollars
+    total = (.25 * quarters) + (.10 * dimes) + (.05 * nickels) + (.01 * pennies)
+    return round(total,2)
+
+def make_coffee(drink_name,ingredients):
+    for item in ingredients:
+        resources[item] -= ingredients[item]
+    print(f"Here is your {drink_name}☕ Enjoy!")
+
+def check_resources(ingredient):
+    for item in ingredient:
+        if ingredient[item] > resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+
+    return True
 
 
-machine_off = False
-print(report())
-print(f"Money: {money}")
-choice = ask_user()
-if choice == "report":
-    print(report())
-elif choice == "off":
-    machine_off = True
-else:
-    print("Please insert coins:")
-    total = round(insert_coin(),2)
-    print(f"Inserted Money: {total}")
-    cost = MENU[choice]["cost"]
-    if cost < total:
-        change = total - cost
-        print(f"Change: {change}")
-        print(f"Here is your {choice}☕ Enjoy!")
+machine_on = True
+while machine_on:
+    choice = input("What would you like? (espresso/latte/cappuccino):").lower()
+    if choice == "off":
+        machine_on = False
+    elif choice == "report":
+        report()
     else:
-        print(f"You don't have enough cash to buy {choice}.")
+        drink = MENU[choice]
+        ingredients = drink["ingredients"]
+        cost = drink["cost"]
+
+        if check_resources(ingredients):
+            payment = insert_coin()
+
+            if transaction_succesful(payment,cost):
+                money += cost
+                make_coffee(choice,ingredients)
+
