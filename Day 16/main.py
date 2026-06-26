5e58f81e8c1563cca2afe1629b81aadaf594d822
Joel Davis <joeldavis1706@gmail.com>
2026-06-27 03:43:09 +0530

    Completed Day 16 Coffee Maker using OOP

---
+++ Day 16/main.py
@@ -0,0 +1,22 @@
+from menu import Menu,MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+money_machine.report()
+
+machine_on = True
+while machine_on:
+    choice = input(f"What would you like? {menu.get_items()} ?:").lower()
+    if choice == "off":
+        machine_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(drink.ingredients["water"])
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
